aa68c29ed184527c3ccdff04f701d9566da8c6dd
bertrand12345 <data@batanganaekobo.ch>
2025-10-17 05:26:29 -0400

    twenty_one commit

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 8 */
+/* jshint esversion: 8 */
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
@@ -6,25 +6,29 @@ import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 import Profile from './components/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 function App() {
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
+
   return (
-        <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/product/:productId" element={<DetailsPage/>} />
-          <Route path="/app/search" element={<SearchPage/>} />
-          <Route path="/app/profile" element={<Profile/>} />
-        </Routes>
-        </>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/app" element={<MainPage />} />
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
+        <Route path="/app/product/:productId" element={<DetailsPage />} />
+        <Route path="/app/search" element={<SearchPage />} />
+        <Route path="/app/profile" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
+
 export default App;
